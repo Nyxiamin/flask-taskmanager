f88b218600b4516c8926f93e71251dc986958ee1
Thaïs <104143788+Nyxiamin@users.noreply.github.com>
2025-12-02 13:35:22 +0100

    Triggering workflow 7

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -4,8 +4,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+service = ChromeService(executable_path="/usr/bin/chromedriver")
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 APP_URL = "http://127.0.0.1:5000"
 
